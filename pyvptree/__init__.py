e2cec4d468f750e72d525c6ccc561e677c15d8a7
Pablo Elias <pablo.elias@woven-planet.global>
2023-08-13 10:55:49 +0900

    fix: added extra dimensionality check for binindex

--- pyvptree/__init__.py
+++ pyvptree/__init__.py
@@ -9,9 +9,12 @@ from _pyvptree import VPTreeL2Index
 
 from ._version import __version__
 
+
 class VPTreeBinaryIndex:
+
     def __init__(self):
         self._index = None
+        self._dimension = None
 
     def set(self, data):
         self._validate(data)
@@ -26,25 +29,34 @@ class VPTreeBinaryIndex:
         elif dim == 8:
             self._index = VPTreeBinaryIndex64()
 
+        self._dimension = dim
         self._index.set(data)
 
-
     def searchKNN(self, queries, k):
+        if self._index is None:
+            return [], []
+
+        dim = queries.shape[1]
+        if dim != self._dimension:
+            raise ValueError(f"invalid data dimension: index built data and query data dimensions must agree, index built data dimension is {dim}")
+
         self._validate(queries)
         return self._index.searchKNN(queries, k)
 
     def search1NN(self, queries):
+        if self._index is None:
+            return [], []
+
         self._validate(queries)
         return self._index.search1NN(queries)
 
     def _validate(self, input):
         if len(input.shape) != 2:
-            raise ValueError("Invalid data shape: binary indexes must be 2D")
+            raise ValueError("invalid data shape: binary indexes must be 2D")
 
         if input.dtype != 'uint8':
-            raise TypeError("Invalid data type: binary indexes must be uint8")
+            raise TypeError("invalid data type: binary indexes must be uint8")
 
         dim = input.shape[1]
         if dim not in [8, 16, 32, 64]:
-            raise ValueError("Invalid dimension: binary indexes must be 8, 16, 32 or 64 bytes")
-
+            raise ValueError("invalid dimension: binary indexes must be 8, 16, 32 or 64 bytes")
